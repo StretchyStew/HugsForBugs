2810c52bfdb9c3cb21321c501f620fa3daacafae
StretchyStew <89608584+StretchyStew@users.noreply.github.com>
2023-09-12 22:00:33 -0600

    Finished +, -, *, /, and ( )
    Hopefully finished the + and - operators. Throws exceptions where needed. I need to also add multiplication and division in the else if, so that it follows PEMDAS (only if applicable).

--- Spreadsheet/FormulaEvaluator/Class1.cs
+++ Spreadsheet/FormulaEvaluator/Class1.cs
@@ -17,7 +17,103 @@ public static class Evaluator
             substrings[i] = substrings[i].Trim();
         }
 
-        
+        //creates 2 stacks to hold the values and operators
+        Stack<int> value = new Stack<int>();
+        Stack<string> operators = new Stack<string>();
+
+        for (int i = 0; i < substrings.Length; i++)
+        {
+            string token = substrings[i];
+            double number;
+
+            if (token.Equals("*") || token.Equals("/") || token.Equals("("))
+            {
+                operators.Push(token);
+            }
+
+            else if (token.Equals("+") || token.Equals("-") || token.Equals(")"))
+            {
+                string top = "";
+
+                if (operators.Count > 0)
+                {
+                    top = operators.Peek();
+                }
+
+                if (top.Equals("+") || top.Equals("-"))
+                {
+                    int valOne = value.Pop();
+                    int valTwo = value.Pop();
+                    string operation = operators.Pop();
+
+                    if (operation == "+")
+                    {
+                        value.Push(valOne + valTwo);
+                    }
+                    else
+                    {
+                        value.Push(valOne - valTwo);
+                    }
+                }
+
+                if (top.Equals(")"))
+                {
+                    string newTop = "";
+
+                    if (operators.Count > 0)
+                    {
+                        newTop = operators.Peek();
+                    }
+
+                    //makes sure it is complete i.e. (2+2) and not 2+2)
+                    if (newTop != "(")
+                    {
+                        throw new ArgumentException();
+                    }
+
+                    //removes the '(' from the top
+                    operators.Pop();
+
+                    if (operators.Count > 0)
+                    {
+                        newTop = operators.Peek();
+                    }
+
+                    if (newTop.Equals("*") || newTop.Equals("/"))
+                    {
+                        int valOne = value.Pop();
+                        int valTwo = value.Pop();
+                        string operation = operators.Pop();
+
+                        if (operation.Equals("*"))
+                        {
+                            value.Push(valOne * valTwo);
+                        }
+                        else
+                        {
+                            //divide by zero error
+                            if (valOne == 0)
+                            {
+                                throw new ArgumentException();
+                            }
+                            value.Push(valTwo / valOne);
+                        }
+                    }
+                }
+                else
+                {
+                    operators.Push(token);
+                }
+            }
+
+            //checks to see if our token is a number, if so then do multiplication/division if applicable
+            else if(Double.TryParse(token, out number)){
+                int num = (int) number;
+            }
+
+
+        }
+
         return 0;
     }
 
